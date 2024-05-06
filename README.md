353ba3a97af1e19981e89692ccabc1c3178dcb49
Melissa2002 <mosherof@ucsd.edu>
2024-05-05 17:32:57 -0700

    Add unit tests

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Lab 5 - Starter
 Name: Melissa Osheroff
 Partner: Ruiping Fang
+
+[https://mosherof.github.io/Lab5_Starter/expose.html](https://mosherof.github.io/Lab5_Starter/expose.html)
+
+[https://mosherof.github.io/Lab5_Starter/explore.html](https://mosherof.github.io/Lab5_Starter/explore.html)
+
+1. I would not use a unit test to test the "message" feature of a messaging application because seeing as it needs to account for writing and sending a message to another user, it seems to large scale to debug as a whole. Moreover, this component interacts with other people's applications, and unit testing is unable to test for these types of functions.
+2. Yes, I would use a unit test to test the "max message length" feature because it is something that can be tested small scale, executed quickly, and would likely not be affected by other app features. Additionlly, it can function as an individual component.
+   
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,87 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+ test('tests wrong phone number', () => {
+  expect(isPhoneNumber("4155901")).toBe(false);
+});
+
+test('tests wrong phone number', () => {
+  expect(isPhoneNumber("4155801293")).toBe(false);
+});
+
+test('tests correct phone number', () => {
+  expect(isPhoneNumber("(415)580-1293")).toBe(true);
+});
+
+test('tests correct phone number', () => {
+  expect(isPhoneNumber("(111)111-1293")).toBe(true);
+});
+
+test('tests incorrect email', () => {
+  expect(isEmail("aaa@gmail")).toBe(false);
+});
+
+test('tests incorrect email', () => {
+  expect(isEmail("@gmail.com")).toBe(false);
+});
+
+
+test('tests correct email', () => {
+  expect(isEmail("aaa@gmail.com")).toBe(true);
+});
+
+
+test('tests correct email', () => {
+  expect(isEmail("aaa@ucsd.edu")).toBe(true);
+});
+
+test('tests if not strong password', () => {
+  expect(isStrongPassword("1a_")).toBe(false);
+});
+
+test('tests if not strong password', () => {
+  expect(isStrongPassword("thisistoomanycharactersalthoughtherearenumbersandletters1")).toBe(false);
+});
+
+test('tests if strong password', () => {
+  expect(isStrongPassword("ABC12553")).toBe(true);
+});
+
+test('tests if strong password', () => {
+  expect(isStrongPassword("abc101888")).toBe(true);
+});
+
+test('tests if incorrect date', () => {
+  expect(isDate("011/01/1111")).toBe(false);
+});
+
+test('tests if incorrect date', () => {
+  expect(isDate("01/01/11")).toBe(false);
+});
+
+test('tests if correct date', () => {
+  expect(isDate("01/01/1111")).toBe(true);
+});
+
+test('tests if correct date', () => {
+  expect(isDate("1/1/1111")).toBe(true);
+});
+
+test('tests if incorrect hex color', () => {
+  expect(isHexColor("#1")).toBe(false);
+});
+
+test('tests if incorrect hex color', () => {
+  expect(isHexColor("#1111111")).toBe(false);
+});
+
+test('tests if correct hex color', () => {
+  expect(isHexColor("#FF5733")).toBe(true);
+});
+
+test('tests if correct hex color', () => {
+  expect(isHexColor("#028477")).toBe(true);
+});
+
+
+
